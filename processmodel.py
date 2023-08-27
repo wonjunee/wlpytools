fdecdfc1f0874c38e95bca394da46dbc3dac0c17
Wonjun Lee <mymamyma@gmail.com>
2023-08-27 13:53:12 -0500

    Update processmodel.py

--- processmodel.py
+++ processmodel.py
@@ -1,3 +1,5 @@
+import torch
+
 def saving_models(models, Titlelist, save_data_path, epoch):
     for title in Titlelist:
         torch.save(models[title]['T'].state_dict(), f"{save_data_path}/{epoch}-{title}-T.pt")
@@ -7,3 +9,14 @@ def loading_models(models, Titlelist, save_data_path, epoch):
     for title in Titlelist:
         models[title]['T'].load_state_dict(torch.load(f"{save_data_path}/{epoch}-{title}-T.pt"))
         models[title]['S'].load_state_dict(torch.load(f"{save_data_path}/{epoch}-{title}-S.pt"))
+
+
+
+def get_model(model):
+    T = model['T']
+    S = model['S']
+    phi = model['phi']
+    optT = model['optT']
+    optS = model['optS']
+    optphi = model['optphi']
+    return T,S,phi,optT,optS,optphi
